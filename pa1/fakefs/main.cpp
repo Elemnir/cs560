8b5a67493da2aafdab5659ff7280131a27147d2f
Elemnir <ahoward0920@gmail.com>
2016-02-06 20:13:15 -0500

    Defined shell's expected interface and logic

--- pa1/fakefs/main.cpp
+++ pa1/fakefs/main.cpp
@@ -30,18 +30,76 @@ void parse(const string &str) {
         return;
 
     if (cmd == "mkfs") {
+        fs.make_filesystem();
     } else if (cmd == "open") {
+        if (ss >> sarg1 >> sarg2) {
+            fs.open(sarg1, sarg2);
+        } else {
+            //error
+        }
     } else if (cmd == "read") {
+        if (ss >> fd >> val) {
+            fs.read(fd, val);
+        } else {
+            //error
+        }
     } else if (cmd == "write") {
+        if (ss >> fd >> sarg1) {
+            
+        } else {
+            //error
+        }
     } else if (cmd == "seek") {
+        if (ss >> fd >> val) {
+            fs.seek(fd, val);
+        } else {
+            //error
+        }
     } else if (cmd == "close") {
+        if (ss >> fd) {
+            fs.close(fd);
+        } else {
+            //error
+        }
     } else if (cmd == "mkdir") {
+        if (ss >> sarg1) {
+            fs.mkdir(sarg1);
+        } else {
+            //error
+        }
     } else if (cmd == "rmdir") {
+        if (ss >> sarg1) {
+            fs.rmdir(sarg1);
+        } else {
+            //error
+        }
     } else if (cmd == "cd") {
+        if (ss >> sarg1) {
+            fs.chdir(sarg1);
+        } else {
+            //error
+        }
     } else if (cmd == "ls") {
+        fs.dir_list();
     } else if (cmd == "cat") {
+        if (ss >> sarg1) {
+            fs.dump(sarg1);
+        } else {
+            //error
+        }
     } else if (cmd == "tree") {
+        fs.dir_tree();
     } else if (cmd == "import") {
+        if (ss >> sarg1 >> sarg2) {
+            fs.import(sarg1, sarg2);
+        } else {
+            //error
+        }
     } else if (cmd == "export") {
+        if (ss >> sarg1 >> sarg2) {
+            fs.export(sarg1, sarg2);
+        } else {
+            //error
+        }
     }
 }
