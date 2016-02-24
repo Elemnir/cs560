eb98abdffba8e0ad11070a07dfc1b2f438fc515e
Elemnir <ahoward0920@gmail.com>
2016-02-24 11:59:13 -0500

    Completed Block management and Filesystem initialization

--- pa1/fakefs/filesystem.cpp
+++ pa1/fakefs/filesystem.cpp
@@ -2,10 +2,51 @@
 
 using namespace std;
 
-void FileSystem::load(const string &fsname) {
+FileSystem::FileSystem(const string &fsname) {
+    if (load(fsname)) {
+        get_header();
+        cdi = get_inode(header.root);
+        map_current_dir();
+    }
+}
+
+bool FileSystem::load(const string &fsname) {
+    fsfile.open(fsname, ios_base::in | ios_base::out | ios_base::binary);
+    if (!fsfile.is_open()) {
+        fsfile.clear();
+        fsfile.open(fsname, ios_base::out);
+        fsfile.close();
+        fsfile.open(fsname, ios_base::in | ios_base::out | ios_base::binary);
+        return false;
+    }
+    return true;
 }
 
 void FileSystem::format() {
+    Inode root;
+    FreeListNode flh;
+
+    // Blank the filesystem
+    fsfile.seekp(0);
+    for (int i = 0; i < BLK_SIZE * BLK_COUNT; ++i) {
+        fsfile.put('\0');
+    }
+    fsfile.seekp(0);
+
+    // Initialize the header and freelist
+    get_header();
+    header.flh = sizeof(Header) / BLK_SIZE + 1;
+    flh = get_freelistnode(header.flh);
+    flh.size = header.avail = BLK_COUNT - header.flh;
+    flh.next = 0;
+    set_freelistnode(header.flh, flh);
+    
+    // Initialize the root directory Inode and first Block
+    header.root = allocate_block();
+    init_dir(header.root, header.root);
+    set_header();
+    cdi = get_inode(header.root);
+    map_current_dir();
 }
 
 void FileSystem::open(const string &fname, const string& mode) {
@@ -45,4 +86,97 @@ void FileSystem::import_file(const string &src, const string &dest) {
 }
 
 void FileSystem::export_file(const string &dest, const string &src) {
+}
+
+/* Internals */
+void FileSystem::get_header() {
+    fsfile.seekg(0);
+    fsfile.read((char*) &header, sizeof(Header));
+}
+
+void FileSystem::set_header() {
+    fsfile.seekp(0);
+    fsfile.write((char*) &header, sizeof(Header));
+}
+
+FileSystem::Inode FileSystem::get_inode(const BLK_NO blk) {
+    Inode rval;
+    fsfile.seekg(BLK_SIZE * blk);
+    fsfile.read((char*) &rval, sizeof(Inode));
+    return rval;
+}
+
+void FileSystem::set_inode(const BLK_NO blk, const FileSystem::Inode& buf) {
+    fsfile.seekp(BLK_SIZE * blk);
+    fsfile.write((char*) &buf, sizeof(Inode));
+}
+
+FileSystem::Block FileSystem::get_block(const BLK_NO blk) {
+    Block rval;
+    fsfile.seekg(BLK_SIZE * blk);
+    fsfile.read((char*) &rval, sizeof(Block));
+    return rval;
+}
+
+void FileSystem::set_block(const BLK_NO blk, const FileSystem::Block& buf) {
+    fsfile.seekp(BLK_SIZE * blk);
+    fsfile.write((char*) &buf, sizeof(Block));
+}
+
+FileSystem::FreeListNode FileSystem::get_freelistnode(const BLK_NO blk) {
+    FreeListNode rval;
+    fsfile.seekg(BLK_SIZE * blk);
+    fsfile.read((char*) &rval, sizeof(FreeListNode));
+    return rval;
+}
+
+void FileSystem::set_freelistnode(const BLK_NO blk, 
+        const FileSystem::FreeListNode &buf) {
+    fsfile.seekp(BLK_SIZE * blk);
+    fsfile.write((char*) &buf, sizeof(FreeListNode));
+
+}
+
+BLK_NO FileSystem::allocate_block() {
+    BLK_NO rval = 0;
+    if (header.flh == 0) return rval;
+
+    FreeListNode node = get_freelistnode(header.flh);
+    if (node.size == 1) {
+        rval = header.flh;
+        header.flh = node.next;
+        set_header();
+    } else { 
+        node.size--;
+        rval = header.flh + node.size;
+        set_freelistnode(header.flh, node);
+    }
+    return rval;
+}
+
+void FileSystem::free_block(BLK_NO blk) {
+    FreeListNode node;
+    
+    node.next = header.flh;
+    node.size = 1;
+    header.flh = blk;
+    
+    set_header();
+    set_freelistnode(blk, node);
+}
+
+void FileSystem::init_dir(BLK_NO self, BLK_NO parent) {
+    Inode node = get_inode(self);
+    node.isDir = true;
+    node.blkCount = 1;
+    node.block[0] = allocate_block();
+
+    Block blk = get_block(node.block[0]);
+    node.len = sprintf(&blk, "%x .\n%x ..\n", self, parent);
+
+    set_inode(self, node);
+    set_block(node.block[0], blk);
+}
+
+void FileSystem::map_current_dir() {
 }
--- pa1/fakefs/filesystem.hpp
+++ pa1/fakefs/filesystem.hpp
@@ -1,18 +1,26 @@
 #ifndef FILESYSTEM_HPP
 #define FILESYSTEM_HPP
 
-#include <cstdio>
+#include <fstream>
+#include <map>
 #include <string>
 #include <vector>
+
+#define BLK_SIZE 4096
+#define BLK_COUNT 25600
+
+typedef unsigned int BLK_NO;
+typedef unsigned int BLK_CT;
+typedef unsigned int OFFSET;
 
 using namespace std;
 
 class FileSystem {
   public:
     FileSystem() {};
-    FileSystem(const string& fsname) { load(fsname); }
+    FileSystem(const string& fsname);
 
-    void load(const string &fname);
+    bool load(const string &fname);
     void format();
     void open(const string &fname, const string& mode);
     void read(int fd, int size);
@@ -29,14 +37,67 @@ class FileSystem {
     void export_file(const string &dest, const string &src);
   
   protected:
+    
+    struct Header {
+        BLK_NO root;
+        BLK_NO flh;
+        BLK_CT avail;
+    };
+
+    struct FreeListNode {
+        BLK_NO next;
+        BLK_CT size;
+    };
+
+    struct Inode {
+        BLK_CT blkCount;
+        size_t len;
+        bool isDir, isBig;
+        BLK_NO block[512];
+    };
+
+    struct Block {
+        char* operator&() { return text; }
+        char& operator[](int i) { return text[i]; }
+        char text[BLK_SIZE];
+    };
+    
     struct FileHandle {
         FileHandle() : seek(0) {};
-        size_t seek;
+        OFFSET seek;
+        BLK_NO inode;
+        Block curr;
+        bool read, write;
     };
 
   private:
-    FILE *_fsfile;
-    vector<FileHandle> _active;
+
+    // Setters and Getters for the Header, Inodes, Blocks, and FreeListNodes
+    void    get_header();
+    void    set_header();
+    Inode   get_inode(const BLK_NO blk);
+    void    set_inode(const BLK_NO blk, const Inode& buf);    
+    Block   get_block(const BLK_NO blk);
+    void    set_block(const BLK_NO blk, const Block& buf);
+    FreeListNode get_freelistnode(const BLK_NO blk);
+    void  set_freelistnode(const BLK_NO blk, const FreeListNode& buf);
+    
+    // Use for selecting and returning blocks from the free set
+    BLK_NO allocate_block();
+    void   free_block(BLK_NO blk);
+
+    // Call whenever modifying the current directory
+    void init_dir(BLK_NO self, BLK_NO parent);
+    void map_current_dir();
+
+    // Handle to storage, the FS Header, and the Current Directory
+    fstream fsfile;
+    Header header;
+    Inode cdi;
+
+    // Files that are open, and the contents of the current directory
+    map<string, BLK_NO> cdd;
+    vector<FileHandle> opened;
 };
 
 #endif // FILESYSTEM_HPP
